2ee13ec00fca62d58fdf97ddbfe3749dae0bcf8a
Thomas Hooper <tom@stainlessvision.com>
2015-11-28 13:19:47 +0000

    Basic front/back geometry

--- src/slice.js
+++ src/slice.js
@@ -1,1 +1,62 @@
-THREE.slice = 'foo'
+(function() {
+
+    var FRONT = 'front';
+    var BACK = 'back';
+
+    window.sliceGeometry = function(geom, plane) {
+        var sliced = new THREE.Geometry();
+        var vertices;
+        var position;
+        geom.faces.forEach(function(face) {
+            vertices = faceVertices(geom, face);
+            position = facePosition(plane, vertices);
+            if (position == FRONT) {
+                addFace(sliced, vertices);
+            }
+        });
+        return sliced;
+    };
+
+    var addFace = function(geom, vertices) {
+        var existingIndex;
+        var vertexIndices = [];
+        var indexOffset = geom.vertices.length;
+        var exists;
+
+        vertices.forEach(function(vertex) {
+            existingIndex = geom.vertices.indexOf(vertex);
+            if (existingIndex !== -1) {
+                vertexIndices.push(existingIndex);
+            } else {
+                geom.vertices.push(vertex);
+                vertexIndices.push(indexOffset);
+                indexOffset += 1;
+            }
+            return ! exists;
+        });
+
+        var face = new THREE.Face3(
+            vertexIndices[0],
+            vertexIndices[1],
+            vertexIndices[2]
+        );
+        geom.faces.push(face);
+    };
+
+    var faceVertices = function(geom, face) {
+        return ['a', 'b', 'c'].map(function(key) {
+            return geom.vertices[face[key]];
+        });
+    }
+
+    var facePosition = function(plane, vertices) {
+        var position = FRONT;
+        vertices.forEach(function(vertex) {
+            if (plane.distanceToPoint(vertex) < 0) {
+                position = BACK;
+            }
+        });
+        return position;
+    };
+
+})()
--- src/test/slice.spec.js
+++ src/test/slice.spec.js
@@ -1,7 +1,82 @@
-describe("Slice", function() {
+describe("three.js slice geometry", function() {
 
-    it("adds a slice method to THREE", function() {
-        expect(THREE.slice).not.toBe(undefined);
+    var geometry;
+
+    describe("a single face", function() {
+
+        beforeEach(function() {
+            geometry = new THREE.Geometry();
+
+            geometry.vertices = [
+                new THREE.Vector3(0, 0, 0),
+                new THREE.Vector3(1, 0, 0),
+                new THREE.Vector3(0, 1, 0)
+            ];
+
+            geometry.faces = [
+                new THREE.Face3(0, 1, 2)
+            ];
+        });
+
+        it("sliced with whole geometry behind plane", function() {
+            var plane = new THREE.Plane(
+                new THREE.Vector3(1, 0, 0),
+                -2
+            )
+            var sliced = sliceGeometry(geometry, plane);
+            expect(sliced.vertices.length).toBe(0);
+            expect(sliced.faces.length).toBe(0);
+        });
+
+        it("sliced with whole geometry in front of plane", function() {
+            var plane = new THREE.Plane(
+                new THREE.Vector3(1, 0, 0),
+                2
+            )
+            var sliced = sliceGeometry(geometry, plane);
+            expect(sliced.vertices).toEqual(geometry.vertices);
+            expect(sliced.faces).toEqual(geometry.faces);
+        });
+
     });
 
+    describe("two faces", function() {
+
+        beforeEach(function() {
+            geometry = new THREE.Geometry();
+
+            geometry.vertices = [
+                new THREE.Vector3(0, 0, 0),
+                new THREE.Vector3(1, 0, 0),
+                new THREE.Vector3(0, 1, 0),
+                new THREE.Vector3(1, 1, 0)
+            ];
+
+            geometry.faces = [
+                new THREE.Face3(0, 1, 2),
+                new THREE.Face3(3, 2, 1)
+            ];
+        });
+
+        it("sliced with whole geometry behind plane", function() {
+            var plane = new THREE.Plane(
+                new THREE.Vector3(1, 0, 0),
+                -2
+            )
+            var sliced = sliceGeometry(geometry, plane);
+            expect(sliced.vertices.length).toBe(0);
+            expect(sliced.faces.length).toBe(0);
+        });
+
+        it("sliced with whole geometry in front of plane", function() {
+            var plane = new THREE.Plane(
+                new THREE.Vector3(1, 0, 0),
+                2
+            )
+            var sliced = sliceGeometry(geometry, plane);
+            expect(sliced.vertices).toEqual(geometry.vertices);
+            expect(sliced.faces).toEqual(geometry.faces);
+        });
+
+    });
 });
