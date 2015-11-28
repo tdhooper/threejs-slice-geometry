90e8d62ff107608ce238b172749c6e500f159db1
Thomas Hooper <tom@stainlessvision.com>
2015-11-28 16:46:15 +0000

    Slice triangles

--- src/slice.js
+++ src/slice.js
@@ -3,6 +3,8 @@
 
     var FRONT = 'front';
     var BACK = 'back';
+    var STRADDLE = 'straddle';
+    var ON = 'on';
 
     window.sliceGeometry = function(geom, plane) {
         var sliced = new THREE.Geometry();
@@ -13,9 +15,36 @@
             position = facePosition(plane, vertices);
             if (position == FRONT) {
                 addFace(sliced, vertices);
+            } else if (position == STRADDLE) {
+                sliceFace(plane, sliced, vertices);
             }
         });
         return sliced;
+    };
+
+    var sliceFace = function(plane, geom, vertices) {
+        var i;
+        var len = vertices.length;
+        var v1;
+        var v2;
+        var intersection;
+        var position;
+        var sliceVertices = [];
+
+        for (i = 0; i < len; i++) {
+            v1 = vertices[i];
+            v2 = i + 1 < len ? vertices[i + 1] : vertices[0];
+            intersection = intersectPlane(v1, v2, plane);
+            position = vertexPosition(plane, v1);
+            if (position == FRONT) {
+                sliceVertices.push(v1);
+            }
+            if (intersection) {
+                sliceVertices.push(intersection);
+            }
+        }
+
+        addFace(geom, sliceVertices);
     };
 
     var addFace = function(geom, vertices) {
@@ -50,14 +79,39 @@
         });
     };
 
+    var intersectPlane = function(v1, v2, plane) {
+        var line = new THREE.Line3(v1, v2);
+        return plane.intersectLine(line);
+    };
+
     var facePosition = function(plane, vertices) {
-        var position = FRONT;
-        vertices.forEach(function(vertex) {
-            if (plane.distanceToPoint(vertex) < 0) {
-                position = BACK;
+        var a = vertexPosition(plane, vertices[0]);
+        var b = vertexPosition(plane, vertices[1]);
+        var c = vertexPosition(plane, vertices[2]);
+        if (a == BACK || b == BACK || c == BACK) {
+            if (a == FRONT || b == FRONT || c == FRONT) {
+                return STRADDLE;
             }
-        });
-        return position;
+            return BACK;
+        }
+        if (a == FRONT || b == FRONT || c == FRONT) {
+            if (a == BACK || b == BACK || c == BACK) {
+                return STRADDLE;
+            }
+            return FRONT;
+        }
+        return ON;
+    };
+
+    var vertexPosition = function(plane, vertex) {
+        var distance = plane.distanceToPoint(vertex);
+        if (distance < 0) {
+            return BACK;
+        }
+        if (distance > 0) {
+            return FRONT;
+        }
+        return ON;
     };
 
 })();
--- src/test/slice.spec.js
+++ src/test/slice.spec.js
@@ -11,6 +11,69 @@ describe("three.js slice geometry", function() {
         });
     };
 
+    var faceVerticesAndNormals = function(geom) {
+        var geomCopy = geom.clone();
+        var vertices;
+        geomCopy.computeFaceNormals();
+        var compareArray = function(a, b) {
+            for (var i = 0; i < a.length; i++) {
+                if (a[i] !== b[i]) {
+                    return a[i] - b[i];
+                }
+            }
+            return 0;
+        };
+        return geomCopy.faces.map(function(face) {
+            vertices = ['a', 'b', 'c'].map(function(key) {
+                return geomCopy.vertices[face[key]].toArray();
+            }).sort(compareArray);
+            return {
+                vertices: vertices,
+                normal: face.normal.toArray()
+            };
+        }).sort();
+    };
+
+    beforeEach(function() {
+        jasmine.addMatchers({
+            objectToBeCloseTo: function(util, customEqualityTesters) {
+                var iterate = function(actual, expected, precision) {
+                    var result = true;
+                    var equal;
+                    for (var property in actual) {
+                        if (actual.hasOwnProperty(property)) {
+                            if (typeof actual[property] == "object") {
+                                result = iterate(actual[property], expected[property], precision);
+                            } else {
+                                equal = Math.abs(expected[property] - actual[property]) < (Math.pow(10, -precision) / 2);
+                                if ( ! equal) {
+                                    result = "Expected " + actual[property] + " to be close to " + expected[property];
+                                }
+                            }
+                            if (result !== true) {
+                                return result;
+                            }
+                        }
+                    }
+                    return result;
+                };
+                return {
+                    compare: function(actual, expected, precision) {
+                        var result = iterate(actual, expected, precision);
+                        if (result !== true) {
+                            return {
+                                pass: false,
+                                message: result
+                            };
+                        }
+                        return {
+                            pass: true
+                        };
+                    }
+                };
+            }
+        });
+    });
 
     describe("a single face", function() {
 
@@ -48,7 +111,6 @@ describe("three.js slice geometry", function() {
             expect(sliced.faces).toEqual(geometry.faces);
         });
 
-
         it("sliced with whole geometry behind and touching plane", function() {
             var plane = new THREE.Plane(
                 new THREE.Vector3(-1, 0, 0),
@@ -67,6 +129,24 @@ describe("three.js slice geometry", function() {
             var sliced = sliceGeometry(geometry, plane);
             expect(sliced.vertices).toEqual(geometry.vertices);
             expect(sliced.faces).toEqual(geometry.faces);
+        });
+
+        it("sliced with one vertex in front of plane", function() {
+            var plane = new THREE.Plane(
+                new THREE.Vector3(1, 0, 0).normalize(),
+                -0.5
+            );
+            var sliced = sliceGeometry(geometry, plane);
+            var expected = new THREE.Geometry();
+            expected.vertices = [
+                new THREE.Vector3(0.5, 0, 0),
+                new THREE.Vector3(1, 0, 0),
+                new THREE.Vector3(0.5, 0.5, 0)
+            ];
+            expected.faces = [
+                new THREE.Face3(0, 1, 2)
+            ];
+            expect(faceVerticesAndNormals(sliced)).toEqual(faceVerticesAndNormals(expected));
         });
     });
 
@@ -112,17 +192,19 @@ describe("three.js slice geometry", function() {
         it("sliced with one face on either side", function() {
             var plane = new THREE.Plane(
                 new THREE.Vector3(1, 1, 0).normalize(),
-                -0.5
+                Math.sqrt(2) / -2
             );
             var sliced = sliceGeometry(geometry, plane);
-            expect(sliced.vertices.length).toBe(3);
-            expect(faceVertices(sliced)).toEqual([
-                [
-                    new THREE.Vector3(1, 1, 0),
-                    new THREE.Vector3(0, 1, 0),
-                    new THREE.Vector3(1, 0, 0)
-                ]
-            ]);
+            var expected = new THREE.Geometry();
+            expected.vertices = [
+                new THREE.Vector3(1, 1, 0),
+                new THREE.Vector3(0, 1, 0),
+                new THREE.Vector3(1, 0, 0)
+            ];
+            expected.faces = [
+                new THREE.Face3(0, 1, 2)
+            ];
+            expect(faceVerticesAndNormals(sliced)).objectToBeCloseTo(faceVerticesAndNormals(expected), 2);
         });
     });
 });
