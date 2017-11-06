5fb3f799473f5283c04e2e47bf2613c22b222631
Thomas Hooper <tom@stainlessvision.com>
2017-11-06 01:27:01 +0100

    Simplify faces from edges
    Work with collapsed edges (a list of points), instead of a
    list of edges.
    
    Make all functions pure

--- src/faces-from-edges.js
+++ src/faces-from-edges.js
@@ -1,88 +1,83 @@
 
 function facesFromEdges(edges) {
     var chains = joinEdges(edges).filter(validFace);
-    var faces = chains.map(function(chain) {
-        return chain.map(function(edge) {
-            return edge[0];
-        });
-    });
-    return faces;
-}
-
-function joinEdges(edges) {
-    changes = true;
-    var chains = edges.map(function(edge) {
-        return [edge];
-    });
-    while (changes) {
-        changes = connectChains(chains);
-    }
-    chains = chains.filter(function(chain) {
-        return chain.length; 
-    });
+    chains = chains.map(function(chain) {
+        return chain.slice(1);
+    })
     return chains;
 }
 
+function joinEdges(edges) {
+    var lastConnected;
+    var connected = edges;
+    while (connected !== lastConnected) {
+        lastConnected = connected;
+        connected = connectChains(lastConnected);
+    }
+    return connected;
+}
+
 function connectChains(chains) {
-    chains.forEach(function(chainA, i) {
-        chains.forEach(function(chainB, j) {
-            var merged = mergeChains(chainA, chainB);
-            if (merged) {
-                delete chains[j];
-                return true;
+    var len = chains.length;
+    for (var i = 0; i < len; i++) {
+        for (var j = 0; j < len; j++) {
+            if (i === j) {
+                continue;
             }
-        });
-    });
-    return false;
+            var merged = mergeChains(chains[i], chains[j]);
+            if (merged) {
+                chains = chains.filter(function(_, k) {
+                    return k !== i && k !== j;
+                });
+                chains.unshift(merged);
+                return chains;
+            }
+        }
+    }
+    return chains;
 }
 
 function mergeChains(chainA, chainB) {
 
-    if (chainA === chainB) {
-        return false;
-    }
-
-    if (chainStart(chainA) === chainEnd(chainB)) {
-        chainA.unshift.apply(chainA, chainB);
-        return true;
-    }
-
-    if (chainStart(chainA) === chainStart(chainB)) {
-        reverseChain(chainB);
-        chainA.unshift.apply(chainA, chainB);
-        return true;
+    if (chainEnd(chainB) === chainStart(chainA)) {
+        var chain = chainB.slice(0,-1);
+        chain.push.apply(chain, chainA);
+        return chain;
     }
 
     if (chainEnd(chainA) === chainStart(chainB)) {
-        chainA.push.apply(chainA, chainB);
-        return true;
+        var chain = chainA.slice(0,-1);
+        chain.push.apply(chain, chainB);
+        return chain;
+    }
+
+    if (chainStart(chainA) === chainStart(chainB)) {
+        var chain = chainB.slice(1).reverse();
+        chain.push.apply(chain, chainA);
+        return chain;
     }
 
     if (chainEnd(chainA) === chainEnd(chainB)) {
-        reverseChain(chainB);
-        chainA.push.apply(chainA, chainB);
-        return true;
+        var chain = chainA.slice(0,-1).reverse();
+        chain.unshift.apply(chain, chainB);
+        return chain;
     }
 
     return false;
 }
 
 function chainStart(chain){
-    return chain[0][0];
+    return chain[0];
 }
 
 function chainEnd(chain) {
-    return chain[chain.length - 1][1];
-}
-
-function reverseChain(chain) {
-    chain.reverse();
-    chain.forEach(function(edge) {
-        edge.reverse();
-    });
+    return chain[chain.length - 1];
 }
 
 function validFace(chain) {
+    if (chain.length < 3) {
+        return 0;
+    }
     return chainStart(chain) === chainEnd(chain) ? 1 : 0;
 }
 
